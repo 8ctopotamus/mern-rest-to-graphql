eb28171b8eabc1dd87252007aefb1b275915c2fc
Josh <zylo.codes@gmail.com>
2021-12-18 11:58:51 -0600

    Adds type Query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,15 @@
-const resolvers = {
+const { Matchup, Tech } = require('../models')
 
+const resolvers = {
+  Query: {
+    tech: async () => {
+      return await Tech.find({})
+    },
+    matchups: async (parent, { _id }) => {
+      const findQuery = _id ? { _id } : {}
+      return await Matchup.find(findQuery)
+    }
+  }
 }
 
 module.exports = resolvers
