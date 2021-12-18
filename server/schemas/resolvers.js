3b34db815308133f027412221df339bbcd4fb472
Josh <zylo.codes@gmail.com>
2021-12-18 12:17:42 -0600

    type Mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,6 +9,19 @@ const resolvers = {
       const findQuery = _id ? { _id } : {}
       return await Matchup.find(findQuery)
     }
+  },
+  Mutation: {
+    createMatchup: async (parent, args) => {
+      return await Matchup.create(args)
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const updatedMatchup = await Matchup.findOneAndUpdate(
+        { _id: _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return updatedMatchup
+    }
   }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,6 +18,11 @@ const typeDefs = gql`
     tech: [Tech]
     matchups(_id: String): [Matchup]
   }
+
+  type Mutation {
+    createMatchup(tech1: String!, tech2: String!) : Matchup
+    createVote(_id: String!, techNum: Int!) : Matchup
+  }
 `
 
 module.exports = typeDefs
