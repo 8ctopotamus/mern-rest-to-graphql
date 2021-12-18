1add47a8cff1d0d4e8d2e5198715fc5b2c133ae5
Josh <zylo.codes@gmail.com>
2021-12-18 11:48:12 -0600

    Implements ApolloServer and typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+  type Tech {
+    _id: ID!
+    name: String!
+  }
+
+  type Matchup {
+    _id: ID!
+    tech1: String!
+    tech2: String!
+    tech1_votes: Int
+    tech2_votes: Int
+  }
+
+  type Query {
+    tech: [Tech]
+    matchups(_id: String): [Matchup]
+  }
+`
+
+module.exports = typeDefs
