58823c577d9e6728780b6c0eaeed4daaca67775b
Josh <zylo.codes@gmail.com>
2023-01-30 19:45:18 -0600

    Sets up ApolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Matchup from './pages/Matchup';
 import Vote from './pages/Vote';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/matchup" component={Matchup} />
-        <Route exact path="/matchup/:id" component={Vote} />
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/matchup" component={Matchup} />
+          <Route exact path="/matchup/:id" component={Vote} />
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
